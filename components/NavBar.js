ae57ef862a2e7958bf512856f12aa94bfb025cf7
ClarXo <cheonaru@gmail.com>
2022-02-01 23:29:02 +0900

    style(styled-jsx): styled jsx applied only to scope

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import styles from './NavBar.module.css';
 
 const NavBar = (props) => {
   const router = useRouter();
@@ -9,11 +8,22 @@ const NavBar = (props) => {
   return (
     <nav>
       <Link href={'/'}>
-        <a className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`}>Home</a>
+        <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
       </Link>
       <Link href={'/about'}>
-        <a className={[styles.link, router.pathname === '/about' ? styles.active : ''].join(' ')}>About</a>
+        <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 };
